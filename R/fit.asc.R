1eaaedd0895c3dc1f347478d8ef03f806044e77f
Tom Constant <t.j.constant@gmail.com>
2015-07-02 10:19:49 +0100

    include FWHM in output of fit.asc, and increase range of plot to spectrometer limits.

--- R/fit.asc.R
+++ R/fit.asc.R
@@ -10,9 +10,11 @@ fit.asc <-function(asc){
   
   plot(asc$data,pch=16,xlab=label_wavelength(si_prefix = "n"),ylab="counts")
   
-  plot.function(function(x) max(asc$data$y)*exp(-(x - par[2])^2/(2 * par[1]^2)),from=350,to=750,n=600,add=T,col=2,lwd=2)
+  plot.function(function(x) max(asc$data$y)*exp(-(x - par[2])^2/(2 * par[1]^2)),from=350,to=950,n=800,add=T,col=2,lwd=2)
   
   abline(v=par[2],col=2,lty=2)
   
-  kable(data.frame(lambda_0=par[2],sd=par[1]))
+  kable(data.frame(lambda_0=par[2],sd=par[1],fwhm=2*sqrt(2*log(2))*par[1]))
+  
+  return(data.frame(lambda_0=par[2],sd=par[1],fwhm=2*sqrt(2*log(2))*par[1]))
 }
